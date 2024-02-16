1ec5f05a9cd5af9f47e2fce0bf78a21231a8f21d
Federico Moret <106476837+shlimsady@users.noreply.github.com>
2024-02-16 19:48:33 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -1,11 +1,56 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = function(e) {
+  let who = [
+    "he",
+    "she",
+    "they",
+    "it",
+    "the aliens",
+    "the monster",
+    "the ghost"
+  ];
+  let action = [
+    "ate",
+    "threw",
+    "destroyed",
+    "burned",
+    "tore apart",
+    "swallowed",
+    "vaporized"
+  ];
+  let what = [
+    "my arms",
+    "my dog",
+    "my homework",
+    "my car",
+    "my lunch",
+    "my computer"
+  ];
+  let when = [
+    "yesterday",
+    "today",
+    "tomorrow",
+    "last week",
+    "last month",
+    "next year"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // Add event listener to the button
+  document
+    .getElementById("generateButton")
+    .addEventListener("click", function() {
+      const stat = generateRandomExcuse(who, action, what, when);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+      const statElement = document.getElementById("excuse"); // Changed to "excuse" to match HTML
+
+      statElement.innerHTML = "<span style='color:red'>" + stat + "</span>";
+    });
 };
+
+function generateRandomExcuse(who, action, what, when) {
+  const whoRandom = who[Math.floor(Math.random() * who.length)];
+  const actionRandom = action[Math.floor(Math.random() * action.length)];
+  const whatRandom = what[Math.floor(Math.random() * what.length)];
+  const whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  return whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+}
